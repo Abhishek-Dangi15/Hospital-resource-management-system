aa41cb16bd6575784664df0ec54878bbe57f9df9
Abhishek-Dangi15 <thakurabhishek7629@gmail.com>
2025-05-07 22:15:46 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,42 +1,54 @@
 import express from "express";
 import { config } from "dotenv";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
-import userRouter from "./router/userRouter.js"
-import messageRouter from "./router/messageRouter.js"
-import appointmentRouter from "./router/appointmentRouter.js"
+import userRouter from "./router/userRouter.js";
+import messageRouter from "./router/messageRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 
+config();
 
-config({ path: "./config/config.env" })
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.log("Request origin:", origin);
+      const allowedOrigins = [
+        process.env.FRONTEND_PATIENT,
+        process.env.FRONTEND_ADMIN,
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.use(cors(
-    {
-        origin: [process.env.FRONTEND_PATIENT, process.env.FRONTEND_ADMIN],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true
-    }
-))
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir: '/tmp/'
-}))
+    tempFileDir: "/tmp/",
+  })
+);
 
-app.use("/api/v1/message", messageRouter)
-app.use("/api/v1/user", userRouter)
-app.use("/api/v1/appointment", appointmentRouter)
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 
 dbConnection();
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 export default app;
